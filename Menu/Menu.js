5c081019a3561e32489e40c71aaf230855853945
Joshua Gralewski <jgralews3@yahoo.com>
2020-06-09 01:01:57 -0500

    MVP finished

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -31,3 +31,21 @@ let menuItems = [
 
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned markup to the DOM.
 */
+
+function menuMaker (items){
+  let menuContainer = document.createElement('div');
+  let menu = document.createElement ('ul');
+  let menuButton = document.querySelector ('.menu-button');
+
+  items.forEach ((item) => {let menuItem = document.createElement ('li');
+  menuItem.textContent = item;
+  menuContainer.appendChild(menu);
+  menu.appendChild(menuItem);})
+  
+  menuContainer.classList.add ('menu');
+  menuButton.addEventListener ('click', ()=>{menuContainer.classList.toggle("menu--open");})
+
+  return menuContainer;
+}
+let header1 = document.querySelector ('.articles');
+header1.appendChild (menuMaker (menuItems));
